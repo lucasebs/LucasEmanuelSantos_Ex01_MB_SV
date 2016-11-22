b7ba524b5f700a6e8579996ef63206e11412c590
Lucas Emanuel B Santos <lucasebatista@gmail.com>
2016-11-22 15:20:08 -0300

    Update Design.sv

--- design.sv
+++ design.sv
@@ -1,38 +1,38 @@
+// Code your design here
 module contadorModulo15_1(
   input logic clk, rst,
-  output logic cont);
+  output logic[3:0] cont);
   
   logic decresce;
   
   always_ff@(posedge clk or posedge rst)
     if (rst)
       begin
-        decresce = 0
-        cont <= 4'd0;
+        decresce = 0;
+        cont = 4'd0;
       end
   
   	else
       begin
         
         if (decresce)
-          
           if (cont == 4'd0)
             begin
               decresce = 0;
-              cont <= cont + 4'd1;
+              cont = cont + 4'd1;
             end
           else
-            cont <= cont - 4'd1;
+            cont = cont - 4'd1;
         
       	else
           
-          if (cont == 4'd0)
+          if (cont == 4'd15)
             begin
               decresce = 1;
-              cont <= cont - 4'd1;
+              cont = cont - 4'd1;
             end
           else
-            cont <= cont + 4'd1;
+            cont = cont + 4'd1;
         
       end
   
@@ -40,15 +40,15 @@ endmodule
   
 module contadorModulo15_2(
   input logic clk, rst,
-  output logic cont);
+  output logic[3:0] cont);
   
   logic decresce;
   
   always_ff@(posedge clk or posedge rst)
     if (rst)
       begin
-        decresce = 0
-        cont <= 4'd0;
+        decresce = 0;
+        cont = 4'd0;
       end
   
   	else
@@ -59,14 +59,14 @@ module contadorModulo15_2(
           if (cont == 4'd0)
               decresce = 0;
           else
-            cont <= cont - 4'd1;
+            cont = cont - 4'd1;
         
       	else
           
-          if (cont == 4'd0)
+          if (cont == 4'd15)
               decresce = 1;
           else
-            cont <= cont + 4'd1;
+            cont = cont + 4'd1;
         
       end
   
